251baa402e0098dd6d98771d3f4e3f995554c2c6
EsterSPR <web.esterspreafico@gmail.com>
2023-01-11 14:49:57 +0100

    completato esercizio

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ const {
 createApp({
     data(){
         return{
-            message: 'Hello!'
+            emails: []
         }
     },
-    methods: {
-        
+    mounted() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.emails.push(result.data.response)
+            })
+        }
     }
 }).mount('#app')
